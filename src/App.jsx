706af743be7017d65201239f460baa0865629247
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-04 22:36:27 -0700

    chore: document token creation

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { Layout } from './views/Layout';
 import { useStateWithStorage } from './utils';
 
 export function App() {
+	// Create a new shopping list with the token 'my test list'
+	// and store it in localStorage at the key 'tcl-shopping-list-token'
 	const [listToken, setListToken] = useStateWithStorage(
 		'my test list',
 		'tcl-shopping-list-token',
