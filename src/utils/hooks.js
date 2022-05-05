42b4a20f9b209bf784713d6122c60f71ea3586b7
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-04 22:15:30 -0700

    chore: document useStateWithStorage

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,15 +1,20 @@
 import { useState, useEffect } from 'react';
 
-export function useStateWithStorage(defaultValue, key) {
+/**
+ * Set some state in React, and also persist that value in localStorage.
+ * @param {string} initialValue The initial value to store in the state.
+ * @param {string} key The key at which we want to store the value.
+ */
+export function useStateWithStorage(initialValue, key) {
 	const [value, setValue] = useState(() => {
-		const stickyValue = localStorage.getItem(key);
-		return stickyValue ? JSON.parse(stickyValue) : defaultValue;
+		const currentValue = localStorage.getItem(key);
+		return currentValue ? currentValue : initialValue;
 	});
 	useEffect(() => {
 		if (value === null || value === undefined) {
 			return localStorage.removeItem(key);
 		}
-		return localStorage.setItem(key, JSON.stringify(value));
+		return localStorage.setItem(key, value);
 	}, [key, value]);
 	return [value, setValue];
 }
