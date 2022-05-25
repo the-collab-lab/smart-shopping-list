9b092b796cf7782ebd76b4c6731020bd60e8b241
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-25 15:19:31 -0700

    feat: change order of hook args

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,10 @@ import { Layout } from './views/Layout';
 import { useStateWithStorage } from './utils';
 
 export function App() {
-	// Create a new shopping list with the token 'my test list'
 	// and store it in localStorage at the key 'tcl-shopping-list-token'
 	const [listToken, setListToken] = useStateWithStorage(
-		'my test list',
 		'tcl-shopping-list-token',
+		null,
 	);
 	return (
 		<Router>
--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -2,19 +2,19 @@ import { useState, useEffect } from 'react';
 
 /**
  * Set some state in React, and also persist that value in localStorage.
- * @param {string} initialValue The initial value to store in the state.
- * @param {string} key The key at which we want to store the value.
+ * @param {string} storageKey The key of the value in localStorage.
+ * @param {string} initialValue The initial value to store in localStorage and React state.
  */
-export function useStateWithStorage(initialValue, key) {
+export function useStateWithStorage(storageKey, initialValue) {
 	const [value, setValue] = useState(() => {
-		const currentValue = localStorage.getItem(key);
+		const currentValue = localStorage.getItem(storageKey);
 		return currentValue ? currentValue : initialValue;
 	});
 	useEffect(() => {
 		if (value === null || value === undefined) {
-			return localStorage.removeItem(key);
+			return localStorage.removeItem(storageKey);
 		}
-		return localStorage.setItem(key, value);
-	}, [key, value]);
+		return localStorage.setItem(storageKey, value);
+	}, [storageKey, value]);
 	return [value, setValue];
 }
