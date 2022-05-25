d93b420c638f5dd07f0407ad6fc9bbeb3c8663b0
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-25 15:08:36 -0700

    chore: change notification copy

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,11 +8,7 @@ export function Home() {
 				I'm the user-friendly onboarding copy that will remain on this page
 				throughout the development of the app.
 			</p>
-			<Notification>
-				I'm a brief set of instructions for the Collabies to read as they
-				onboard to the app. I can be deleted once the Collabies start digging
-				into their issues.
-			</Notification>
+			<Notification>I'm a dismissable notification!</Notification>
 		</div>
 	);
 }
