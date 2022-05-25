8991b96259153324efdb7435926e3222355b61e0
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-25 15:19:47 -0700

    feat: bail if empty token

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -6,6 +6,8 @@ export function List({ listToken }) {
 	const [data, setData] = useState([]);
 
 	useEffect(() => {
+		if (!listToken) return;
+
 		return streamListItems(listToken, (querySnap) => {
 			const nextData = [];
 			querySnap.docs.forEach((docRef) => {
@@ -17,14 +19,14 @@ export function List({ listToken }) {
 		});
 	}, [listToken]);
 
-	const emptyListMessage = <p>Looks like this list is empty!</p>;
+	const emptyListPrompt = <p>Looks like this list is empty!</p>;
 
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
-			{data.length === 0 && emptyListMessage}
+			{listToken && data.length === 0 && emptyListPrompt}
 			<ul>
 				{data.map((item) => {
 					return <ListItem key={item.id} id={item.id} name={item.name} />;
