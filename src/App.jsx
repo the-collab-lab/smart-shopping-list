7959f13f37bcc9e9807ea347bca5740174fec38d
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-25 15:22:45 -0700

    chore: fix explainer for listToken & setListToken

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ import { Layout } from './views/Layout';
 import { useStateWithStorage } from './utils';
 
 export function App() {
-	// and store it in localStorage at the key 'tcl-shopping-list-token'
+	/**
+	 * Here, we're using a custom hook to create `listToken` and a function
+	 * that can be used to update `listToken` later.
+	 *
+	 * `listToken` is `null` by default because brand-new users do not have
+	 * tokens. You'll use `setListToken` later, when you create and update
+	 * the user's list token.
+	 */
 	const [listToken, setListToken] = useStateWithStorage(
 		'tcl-shopping-list-token',
 		null,
