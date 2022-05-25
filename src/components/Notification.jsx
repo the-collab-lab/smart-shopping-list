5e254fa4034c75309e5e1be4d313677db3fb8b45
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-25 15:06:51 -0700

    feat: allow error styling on notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import CloseIcon from '../assets/close-icon.svg';
 import './Notification.css';
 
-export function Notification({ className, children, ...rest }) {
+export function Notification({ className, children, kind, ...rest }) {
 	const [dismissed, setDismissed] = useState(false);
+	let classList = 'Notification';
+	if (kind === 'error') {
+		classList += ' Notification--error';
+	}
 	return (
 		!dismissed && (
-			<div className="Notification" role="status" {...rest}>
+			<div className={classList} role="status" {...rest}>
 				<div className="Notification-body">{children}</div>
 				<button
 					className="Notification-btn"
