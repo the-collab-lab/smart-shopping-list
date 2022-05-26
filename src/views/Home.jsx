a16b9edb38d70e94f2ac298b1a43ff4355f295ab
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-26 11:43:54 -0700

    chore: update homepage copy

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,8 +4,14 @@ export function Home() {
 	return (
 		<div className="Home">
 			<p>
-				I'm the user-friendly onboarding copy that will remain on this page
-				throughout the development of the app.
+				Hello from the home page, and welcome to your Collab Lab project! We've
+				included a few distinct pages (also called views) to get the project
+				started. You can use the nav bar to visit them, but you'll see that they
+				don't do very much yet!
+			</p>
+			<p>
+				It'll be your job, alongside your team, to implement the features this
+				app needs in order to become a working shopping list! Happy coding!
 			</p>
 		</div>
 	);
