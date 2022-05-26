c8e6fb89b37979ed1a4a49898a09d81af7e5a48a
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-05-25 21:03:07 -0700

    feat: add calculateDateDiff function

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,12 +1,21 @@
 export const ONE_DAY_IN_MILLISECONDS = 86400000;
 
 /**
- * Get a new JavaScript Date that is `daysInFuture` days in the future.
+ * Get a new JavaScript Date that is `offset` days in the future.
  * @example
  * // Returns a Date 3 days in the future
  * getFutureDate(3)
- * @param {number} daysInFuture
+ * @param {number} offset
  */
-export function getFutureDate(daysInFuture) {
-	return new Date(Date.now() + daysInFuture * ONE_DAY_IN_MILLISECONDS);
+export function getFutureDate(offset) {
+	return new Date(Date.now() + offset * ONE_DAY_IN_MILLISECONDS);
+}
+
+/**
+ * Calculate the amount of time between two Dates, in milliseconds.
+ * @param {Date} dateA
+ * @param {Date} dateB
+ */
+export function calculateDateDiff(dateA, dateB) {
+	return Math.abs(dateA.getTime() - dateB.getTime());
 }
