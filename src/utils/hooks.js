330d6406302e04defbe1623c6316a252bc8b833b
EJ Mason <13525251+mxmason@users.noreply.github.com>
2023-04-24 00:13:03 -0700

    chore: clean up useStateWithStorage hook

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -2,10 +2,11 @@ import { useState, useEffect } from 'react';
 
 /**
  * Set some state in React, and also persist that value in localStorage.
- * @param {string} initialValue The initial value to store in localStorage and React state.
  * @param {string} storageKey The key of the value in localStorage.
+ * @param {string | null} initialValue The initial value to store in localStorage and React state.
+ * @returns {[string | null, React.Dispatch<string | null>]}
  */
-export function useStateWithStorage(initialValue, storageKey) {
+export function useStateWithStorage(storageKey, initialValue) {
 	const [value, setValue] = useState(() => {
 		const currentValue = localStorage.getItem(storageKey);
 		return currentValue ? currentValue : initialValue;
@@ -16,5 +17,6 @@ export function useStateWithStorage(initialValue, storageKey) {
 		}
 		return localStorage.setItem(storageKey, value);
 	}, [storageKey, value]);
+
 	return [value, setValue];
 }
