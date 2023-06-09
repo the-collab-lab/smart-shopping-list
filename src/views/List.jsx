aa6af21fda21f031140e1e0e1d62b5872a02e343
EJ Mason <13525251+mxmason@users.noreply.github.com>
2023-06-09 09:28:51 -0700

    feat: adjust font sizes

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -8,11 +8,8 @@ export function List({ data }) {
 			</p>
 			<ul>
 				{/**
-				{new Array(100).fill(null).map((_, i) => (
 				 * TODO: write some JavaScript that renders the `data` array
-					<li style={{ fontSize: '2em' }}>item {i}</li>
 				 * using the `ListItem` component that's imported at the top
-				))}
 				 * of this file.
 				 */}
 			</ul>
