7aa23d4995e5800336188a81c2ec154a8a4dd4eb
Britt Danzer <britt.danzer@gmail.com>
2024-04-14 23:43:22 -0500

    fix setup.ts file path

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'jsdom',
-		setupFiles: './tests/setup.ts',
+		setupFiles: './src/tests/setup.ts',
 		server: {
 			// Prevents Vitest from crashing when it
 			// encounters a module that exports CSS.
